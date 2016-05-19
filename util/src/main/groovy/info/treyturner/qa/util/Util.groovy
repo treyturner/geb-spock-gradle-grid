b9651bc8fd963ee1082acdd7e34f8177dc8c9119
Trey Turner <trey.turner@civitaslearning.com>
2016-05-19 13:29:34 -0500

    Adding Util class for platform resolution

---
+++ util/src/main/groovy/info/treyturner/qa/util/Util.groovy
@@ -0,0 +1,27 @@
+package info.treyturner.qa.util
+
+import groovy.json.JsonBuilder
+
+class Util {
+
+    static String getPlatform() {
+        def platform = System.getProperty('os.name')
+        if (platform.toLowerCase().contains('windows')) {
+            if (System.getenv("ProgramFiles(x86)") != null) {
+                return 'windows64'
+            } else {
+                return 'windows32'
+            }
+        } else if (platform.toLowerCase().contains('os x'))
+            return 'osx'
+        else if (platform.contains('64'))
+            return 'linux64'
+        else {
+            return 'linux32'
+        }
+    }
+
+    static String prettyPrint(Object obj) {
+        new JsonBuilder(obj).toPrettyString()
+    }
+}
